482b8688a9850839f14cc935f87532b2c15d1407
dgoshey <dgoshey@demandware.com>
2016-01-05 13:38:54 -0500

    changed port

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ var server = http.createServer(function onRequest(request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write('This is a simple HTTP Server');
     response.end()
-}).listen(3000);
+}).listen(8000);
