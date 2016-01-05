884419de0155b6dc264fac58b83070135f246057
dgoshey <dgoshey@demandware.com>
2016-01-05 14:17:06 -0500

    simplified app.js for test

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-var http = require('http');
-
-var server = http.createServer(function onRequest(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('This is a simple HTTP Server');
-    response.end()
-}).listen(8000);
+Console.log("Hello World")
